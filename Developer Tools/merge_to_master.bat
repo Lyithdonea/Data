55c55bd23d4a5864d2648dae883bc2d027d7e618
MelchiorDahrk <47513579+MelchiorDahrk@users.noreply.github.com>
2025-01-13 21:07:39 -0500

    Update merge_to_master.bat

--- Developer Tools/merge_to_master.bat
+++ Developer Tools/merge_to_master.bat
@@ -1,6 +1,5 @@
 @echo off
-if exist "MD_Azurian Isles.esm" ( del "MD_Azurian Isles.esm" )
-mklink /h "MD_Azurian Isles.esm" "..\\00 Core\\MD_Azurian Isles.esm"
+copy /y "..\\00 Core\\MD_Azurian Isles.esm" "MD_Azurian Isles.esm"
 .\\merge_to_master.exe %1 "MD_Azurian Isles.esm" --overwrite --remove-deleted --apply-moved-references
-move /Y "MD_Azurian Isles.esm" "..\\00 Core\\MD_Azurian Isles.esm"
+move /y "MD_Azurian Isles.esm" "..\\00 Core\\MD_Azurian Isles.esm"
 pause
